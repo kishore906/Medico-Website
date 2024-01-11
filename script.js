c4245ba519db2f6c04ce0f02d735b2935c3ba735
kishore906 <55914125+kishore906@users.noreply.github.com>
2024-01-11 13:18:00 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+// Functionality for sticky header
+
+window.onscroll = function () {
+  myFunction();
+};
+
+let navbar = document.getElementById("header");
+let sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// Functionality for Responsive Navbar
+
+const navBar = document.querySelector(".nav_links");
+
+function openNav() {
+  navBar.style.width = "100%";
+}
+
+function closeNav() {
+  navBar.style.width = "0%";
+}
+
+// Functionality for departments tabs
+function openDepartment(evt, cityName) {
+  let i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+// Swiper Functionality for Gallery Section
+new Swiper(".gallery-slider", {
+  speed: 400,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  slidesPerView: "auto",
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
